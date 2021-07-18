95aa0890e3049e9524ab4f5e62a432ddb9689ba6
Justin Shaber <justinshaber@gmail.com>
2021-07-17 21:37:04 -0700

    Daily Exercise

--- lesson_3/medium/medium2_1.rb
+++ lesson_3/medium/medium2_1.rb
@@ -68,4 +68,29 @@ def fun_with_ids
   puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
 end
 
-fun_with_ids
+def fun_with_ids_2
+  a_outer = 42
+  a_outer_id = a_outer.object_id
+  puts "a_outer is #{a_outer} [42] with an id of: #{a_outer_id} [x] before the block."
+  puts
+
+  1.times do
+    a_outer_inner_id = a_outer.object_id
+
+    puts "a_outer id was #{a_outer_id} [x] before the block and is: #{a_outer_inner_id} [x] inside the block."
+    puts
+
+    a_outer = 22
+
+    puts "a_outer inside after reassignment is #{a_outer} [22] with an id of: #{a_outer_id} [x] before and: #{a_outer.object_id} [y] after."
+    puts
+
+    a_inner = a_outer
+    a_inner_id = a_inner.object_id
+  end
+
+  puts "a_outer is #{a_outer} [42] with an id of: #{a_outer_id} [x] BEFORE and: #{a_outer.object_id} [x] AFTER the block."
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+end
+
+fun_with_ids_2
