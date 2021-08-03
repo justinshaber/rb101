b16cc3c82ab49975d8a510895ecb0821788bf9ca
Justin Shaber <justinshaber@gmail.com>
2021-08-02 20:33:55 -0700

    Lesson 4 quiz

---
+++ lesson_4/lesson_4_quiz/q_4.rb
@@ -0,0 +1,50 @@
+def snake(str)
+  words = str.split
+  current_word = 0
+
+  loop do
+    words[current_word].downcase!
+
+    current_word += 1
+    break if current_word >= words.size
+  end
+
+  words.join('_')
+end
+
+sentence = 'The sky was blue'
+snake(sentence) # => 'the_sky_was_blue'
+
+def camel(str)
+  words = str.split(' ')
+  counter = 0
+
+  while counter < words.size
+    words[counter] = words[counter].capitalize
+
+    counter = counter + 1
+  end
+
+  words.join
+end
+
+sentence = 'The sky was blue'
+camel(sentence) # => 'TheSkyWasBlue'
+
+def upper_snake(str)
+  words = str.split
+  current_word = 0
+
+  loop do
+    break if current_word == words.size
+    
+
+    words[current_word].upcase!
+    current_word += 1
+  end
+
+  words.join('_')
+end
+
+sentence = 'The sky was blue'
+p upper_snake(sentence) # => 'THE_SKY_WAS_BLUE'
