58b183839fa8e1768d1782480c620dca9607d5f2
Justin Shaber <justinshaber@gmail.com>
2021-12-22 14:46:42 -0800

    Assess Prep

---
+++ assess_prep/mutate.rb
@@ -0,0 +1,29 @@
+a = 'name'
+b = 'name'
+c = 'name'
+
+# Are these three local variables pointing to the same object?
+
+a = c # a now points to object referenced by c
+b = a # b now points to object referenced by a, which is object ref'd by c.
+
+puts a.object_id
+puts b.object_id
+puts c.object_id
+
+# What about now?
+a = 5
+b = 5
+c = 5
+
+puts a.object_id
+puts b.object_id
+puts c.object_id
+
+a = :dog
+b = :dog
+c = :dog
+
+puts a.object_id
+puts b.object_id
+puts c.object_id
